d9b167113432fa1ae1bf55bdfe939376120b0f98
Shayan Hooshmand <shayanh454@gmail.com>
2020-11-26 09:42:40 -0800

    added functionality for POS_TAG mode; wrote some tuning for parameter learning functions

--- ngram_model.py
+++ ngram_model.py
@@ -39,6 +39,18 @@ def get_ngrams(line, n) :
     bookended = [('START', 'START')] * max(1, n-1) + line + [('STOP', 'STOP')]
     return [tuple(bookended[i:i+n]) for i in range(len(bookended) - (n - 1))]
 
+def count_words(corpus) :
+    """
+    returns a dictionary of dictionaries that word from tag emissions
+    """
+    word_counts = defaultdict(lambda: defaultdict(int))
+    for line in corpus :
+        for word, tag in line :
+            word_counts[tag][word] += 1
+        
+    return word_counts
+    
+
 class Ngram_model :
 
     def __init__(self, corpus, n=3, POS_TAG=0) :
@@ -79,7 +91,16 @@ class Ngram_model :
         self.ngram_counts = [self.count_ngrams(i) for i in range(1, self.n+1)]
         print("TRAINING CORPUS: NGRAMS COUNTED")
 
+        #count up word probabilites
+        self.word_counts = count_words(self.corpus)
+
+        #initialize beta for backoff prob
+        self.beta = 0.2
+
         return
+    
+    def change_mode(self) :
+        self.POS_TAG = abs(self.POS_TAG - 1)
     
     def get_ngrams(self, line, n) :
         ngrams = []
@@ -126,7 +147,7 @@ class Ngram_model :
         
         return (num_grams * beta) / self.count(ng)
     
-    def backoff_prob(self, ngram, beta) :
+    def backoff_prob(self, ngram, beta=self.beta) :
         
         def get_normalizer(ng) :
             #avoid recalculating
@@ -163,32 +184,66 @@ class Ngram_model :
 
         return alpha * self.backoff_prob(ngram[1:], beta) / normalizer
     
-    def sentence_logprob(self, sentence, beta) :
-        total = 0
-        for ngram in self.get_ngrams(sentence, self.n) :
-            curr = self.backoff_prob(ngram, beta)
-            total += math.log2(curr)
+    def word_prob(self, word, tag) :
+        if word == 'START' or word == 'STOP' :
+            return 1
         
+        #do laplacian smoothing
+        alpha = 0.1
+        tag_count = self.ngram_counts[0][(tag,)]
+        voc_size = len(self.word_counts[tag])
+        return (self.word_counts[tag][word] + alpha) / (tag_count + voc_size * alpha)
+    
+    def sentence_logprob(self, sentence, beta) :
+        #sum ngram probs
+        total = sum(math.log2(self.backoff_prob(ngram, beta)) for ngram in self.get_ngrams(sentence, self.n))
+        #sum word probs, too, if we use POS tags for ngrams
+        if self.POS_TAG : total += sum(math.log2(self.word_prob(word, tag)) for word, tag in sentence)
         return total
 
     def perplexity(self, corpus, beta):
         """
         calculate the log probability of an entire corpus
-        assumes corpus is line by line strings
+        assumes corpus is passed formatted or as line by line
+        strings
         """
-        #preprocess the corpus
-        corpus = [nltk.word_tokenize(line) for line in corpus]
-        print("TEST CORPUS TOKENIZED")
+        #if we are passed an unformatted corpus
+        if(type(corpus[0]) == str) :
+            corpus = self.format_corpus(corpus)
 
-        corpus = pre_process(corpus, self.lexicon, self.POS_TAG)
-        print("TEST CORPUS PREPROCESSED")
         l, M = 0, 0
         for sentence in corpus :
             # add extra 1 to account for start/end tokens in padding trigrams
+            # todo: FIX THIS TO ACCOUNT FOR size of N
             M += len(sentence) + 1
+            if self.POS_TAG : M += len(sentence)
             l += self.sentence_logprob(sentence, beta)
         
         return 2 ** -(l/M)
+    
+    def format_corpus(self, corpus) :
+        """
+        format a corpus which is assumed to be a list of strings
+        (each string a line) into the expectd format for the model
+        """
+        #preprocess the corpus
+        corpus = [nltk.word_tokenize(line) for line in corpus]
+        print("CORPUS TOKENIZED")
+        corpus = pre_process(corpus, self.lexicon, self.POS_TAG)
+        print("CORPUS POS TAGGED")
+        return corpus
+    
+    def tune(self, tuning_corpus) :
+        """
+        learn beta parameter for katz backoff
+        with tuning_corpus assumed to be list of strings
+        (unformatted)
+        """
+        tuning_corpus = self.format_corpus(tuning_corpus)
+        betas = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
+        self.beta = max(betas, lambda beta : self.perplexity(tuning_corpus, beta))
+        print("BETA VALUE LEARNED: ", self.beta)
+
 
 def open_corpus(path) :
     with open(path, 'r') as file :
@@ -196,7 +251,7 @@ def open_corpus(path) :
     return corpus
 
 corpus = open_corpus("./training/formatted_training/training.txt")
-model = Ngram_model(corpus, 3)
+model = Ngram_model(corpus, 3, 1)
 
 
 # corpus = open_corpus("./training/formatted_training/test_training1.txt")
